3a995cd55c8688b34252351dba80bba7c61bc5e9
rrevizorr <udalov2003@rambler.ru>
2024-04-18 00:31:37 +0300

    Рефакторинг

--- src/pages/Offer/index.tsx
+++ src/pages/Offer/index.tsx
@@ -177,7 +177,8 @@ function Offer({ price, name, type, rating, isPremium = false, isFavorite = fals
                   </label>
 
                   <input className="form__rating-input visually-hidden" name="rating" value="4" id="4-stars"
-                    type="radio"/>
+                    type="radio"
+                  />
                   <label htmlFor="4-stars" className="reviews__rating-label form__rating-label" title="good">
                     <svg className="form__star-image" width="37" height="33">
                       <use xlinkHref="#icon-star"></use>
