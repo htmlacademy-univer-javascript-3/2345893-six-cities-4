7b1e8d7aba86a33f7abab593cf8b06c0fa890ec4
rrevizorr <udalov2003@rambler.ru>
2024-04-19 23:49:33 +0300

    Рефакторинг

--- src/pages/404/index.tsx
+++ src/pages/404/index.tsx
@@ -2,7 +2,7 @@ import { NavLink } from 'react-router-dom';
 
 function Page404() {
   return (
-    <div className="page404" style={{textAlign: "center"}}>
+    <div className="page404" style={{textAlign: 'center'}}>
       <h1>404 Not Found</h1>
       <NavLink to="/">Return to Main</NavLink>
     </div>
