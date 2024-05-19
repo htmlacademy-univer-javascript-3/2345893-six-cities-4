6ff5fc955e0892c0dd716ad6de271e56595e49a4
rrevizorr <udalov2003@rambler.ru>
2024-05-19 03:53:49 +0300

    Исправлена сортировка предложений

--- src/helpers/sortOffersByPrice.ts
+++ src/helpers/sortOffersByPrice.ts
@@ -1,5 +1,5 @@
 import type { OffersType } from '../types/OffersType.ts';
 
 export const sortOffersByPrice = (offers: OffersType, reverse = false) => {
-  offers.sort((offer1, offer2) => (offer1.price - offer2.price) * (reverse ? 1 : -1));
+  return [...offers].sort((offer1, offer2) => (offer1.price - offer2.price) * (reverse ? 1 : -1));
 };
--- src/helpers/sortOffersByRating.ts
+++ src/helpers/sortOffersByRating.ts
@@ -1,5 +1,5 @@
 import { OffersType } from '../types/OffersType.ts';
 
 export const sortOffersByRating = (offers: OffersType) => {
-  offers.sort((offer1, offer2) => (offer2.rating - offer1.rating));
+  return [...offers].sort((offer1, offer2) => (offer2.rating - offer1.rating));
 };
